30b262ea916f477214ffa59eb90be5c4a91bef34
Layan11 <layan.haddad0@gmail.com>
2022-03-31 20:03:03 +0300

    backup in case computer die

--- main.py
+++ main.py
@@ -57,21 +57,22 @@ if __name__ == '__main__':
     plt.xticks([]), plt.yticks([])
     plt.show()
 
-    for i in range(width):
-        for j in range(height-300):
-            if canny[i][j] != 0 and canny[i][j+300] != 0:  # edge pixels
-                # print([i, j])
-                # print([i + 200, j])
-                # cv2.circle(canny, ([j, i]), radius=5, color=(255, 255, 255), thickness=-1)
-                # cv2.circle(canny, ([j+200, i]), radius=5, color=(255, 255, 255), thickness=-1)
-                # cv2.imshow('Test image', canny)
-                # cv2.waitKey(0)
-                # plt.subplot(1, 1, 1), plt.imshow(canny, 'gray')
-                # plt.title('errrrrrrrrrr')
-                # plt.xticks([]), plt.yticks([])
-                # plt.show()
+    for k in range(100, 300):
+        for i in range(width):
+            for j in range(height - k):
+                if canny[i][j] != 0 and canny[i][j + k] != 0:  # edge pixels
+                    # print([i, j])
+                    # print([i + 200, j])
+                    # cv2.circle(canny, ([j, i]), radius=5, color=(255, 255, 255), thickness=-1)
+                    # cv2.circle(canny, ([j+200, i]), radius=5, color=(255, 255, 255), thickness=-1)
+                    # # cv2.imshow('Test image', canny)
+                    # # cv2.waitKey(0)
+                    # plt.subplot(1, 1, 1), plt.imshow(canny, 'gray')
+                    # plt.title('errrrrrrrrrr')
+                    # plt.xticks([]), plt.yticks([])
+                    # plt.show()
 
-                indices.append([[i, j], [i, j+300]])
+                    indices.append([[i, j], [i, j + k]])
     # for i in range(tl.shape[0]):
     #     for j in range(tl.shape[1]):
     #         if tl[i][j] != 0:  # edge pixels
@@ -89,18 +90,18 @@ if __name__ == '__main__':
 
         x1, y1 = p1[0], p1[1]
         x2, y2 = p2[0], p2[1]
-        m1 = (x1 + x2)/2
-        m2 = (y1 + y2)/2
+        m1 = int((x1 + x2)/2)
+        m2 = int((y1 + y2)/2)
         # ems.append([x1, y1])
         # ems.append([x2, y2])
-        # ems.append([m2, m1])
-        # for i in range(len(indices)):
-        #     newimage = cv2.circle(canny, (indices[i][0][1], indices[i][0][0]), radius=1, color=(255, 255, 255),thickness=-1)
-
-        # plt.subplot(1, 1, 1), plt.imshow(newimage, 'gray')
-        # plt.title('emsssssss')
-        # plt.xticks([]), plt.yticks([])
-        # plt.show()
+        # ems.append([m1, m2])
+        # for i in range(len(ems)):
+        #     newimage = cv2.circle(canny, (ems[i][1], ems[i][0]), radius=5, color=(255, 255, 255),thickness=-1)
+        #
+        #     plt.subplot(1, 1, 1), plt.imshow(newimage, 'gray')
+        #     plt.title('emsssssss')
+        #     plt.xticks([]), plt.yticks([])
+        #     plt.show()
         XI1 = math.atan2(gY[p1[0]][p1[1]], gX[p1[0]][p1[1]])
         XI2 = math.atan2(gY[p2[0]][p2[1]], gX[p2[0]][p2[1]])
         t1 = (y1 - y2 - x1*XI1 + x2*XI2) / (XI2 - XI1) if (XI2 - XI1) else 1
@@ -116,7 +117,7 @@ if __name__ == '__main__':
             if 0 <= y < cols:
                 accumulatorArray[i][y] += 1
 
-    threshold = 100
+    threshold = max(max(accumulatorArray))
     centers = []
     for i in range(rows):
         for j in range(cols):
@@ -128,8 +129,8 @@ if __name__ == '__main__':
     #     newimage = cv2.circle(img1, (10*centers[i][1], 10*centers[i][0]), radius=5, color=(0, 0, 255), thickness=-1)
     print(len(indices))
     for i in range(len(indices)):
-        newimage = cv2.circle(canny, (indices[i][0][1], indices[i][0][0]), radius=3, color=(255, 255, 255), thickness=-1)
-        newimage = cv2.circle(canny, (indices[i][1][1], indices[i][1][0]), radius=3, color=(255, 255, 255), thickness=-1)
+        newimage = cv2.circle(canny, (indices[i][0][1], indices[i][0][0]), radius=2, color=(255, 255, 255), thickness=-1)
+        newimage = cv2.circle(canny, (indices[i][1][1], indices[i][1][0]), radius=2, color=(255, 255, 255), thickness=-1)
 
     plt.subplot(1, 1, 1), plt.imshow(newimage, 'gray')
     plt.title('all indices')
@@ -137,7 +138,7 @@ if __name__ == '__main__':
     plt.show()
     new = canny
     for i in range(len(centers)):
-        new = cv2.circle(canny, [10*centers[i][0], 10*centers[i][1]], radius=7, color=(255, 255, 255), thickness=-1)
+        new = cv2.circle(canny, [10*centers[i][1], 10*centers[i][0]], radius=5, color=(255, 255, 255), thickness=-1)
 
     plt.subplot(1, 1, 1), plt.imshow(new, 'gray')
     plt.title('winner center points')
