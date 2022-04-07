d85e37b2389931c1db8257d1e4bdc8c41c42a0ff
Layan11 <layan.haddad0@gmail.com>
2022-04-07 17:27:16 +0300

    backup in case computer die

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import plot_implicit, Eq
-from sympy.abc import x, y
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    plot_implicit(Eq(3*x**2+2*x*y+4*y**2, 5))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -53,8 +50,8 @@ if __name__ == '__main__':
     plt.title('Canny')
     plt.xticks([]), plt.yticks([])
     plt.show()
-    k = 70
-    while k <= 250:
+    k = 40
+    while k <= 200:
         for i in range(width):
             for j in range(height - k):
                 if canny[i][j] != 0 and canny[i][j + k] != 0:  # edge pixels
@@ -75,7 +72,7 @@ if __name__ == '__main__':
     for i in range(cols):
         for j in range(rows):
             accumulatorArray[i][j] = 0
-
+    count = 0
     print(len(accumulatorArray))
     for i in range(len(indices)):
         p1 = indices[i][0]
@@ -83,31 +80,38 @@ if __name__ == '__main__':
 
         x1, y1 = p1[0], p1[1]
         x2, y2 = p2[0], p2[1]
-        m1 = int((x1 + x2)/2)
-        m2 = int((y1 + y2)/2)
-        XI1 = math.atan2(gY[p1[0]][p1[1]], gX[p1[0]][p1[1]])
-        XI2 = math.atan2(gY[p2[0]][p2[1]], gX[p2[0]][p2[1]])
+        m1 = round((x1 + x2)/2)
+        m2 = round((y1 + y2)/2)
+        XI1 = math.atan(gY[p1[0]][p1[1]] / gX[p1[0]][p1[1]])
+        XI2 = math.atan(gY[p2[0]][p2[1]] / gX[p2[0]][p2[1]])
         print('XI1 + XI2 = ')
         print(XI1)
         print(XI2)
-        t1 = (y1 - y2 - x1*XI1 + x2*XI2) / (XI2 - XI1) if (XI2 - XI1) else 1
-        t2 = (XI1*XI2*(x2 - x1) - y2*XI1 + y1*XI2) / (XI2 - XI1) if (XI2 - XI1) else 1
+
+        if (XI2 - XI1) == 0:
+            print("they now are zerooooooo")
+            count += 1
+            print(XI2 - XI1)
+        print("THE COUNT ISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+        print(count)
+        t1 = (y1 - y2 - x1*XI2 + x2*XI1) / (XI1 - XI2) if (XI1 - XI2) else 1
+        t2 = (XI1*XI2*(x2 - x1) - y2*XI2 + y1*XI1) / (XI1 - XI2) if (XI1 - XI2) else 1
         m0 = (t2 - m2) / (t1 - m1)
         b0 = (m2*t1 - m1*t2) / (t1 - m1)
 
         m = [m1, m2]
-        start = [int(t2), int(t1)]
+        start = [round(t2), round(t1)]
         x = 10*(cols-1)
         finish = [int(x * (t2 - m2) / (t1 - m1) + (m2 * t1 - m1 * t2) / (t1 - m1)), x]
         # line = cv2.circle(canny, (y1, x1), radius=5, color=(255, 255, 255),thickness=-1)
         # line = cv2.circle(canny, (y2, x2), radius=5, color=(255, 255, 255),thickness=-1)
         # line = cv2.circle(canny, (m2, m1), radius=5, color=(255, 255, 255), thickness=-1)
-        # line = cv2.circle(canny, (int(t2), int(t1)), radius=5, color=(255, 0, 0), thickness=-1)
-        test1 = [p1[1], p1[0]]
-        test2 = [p2[1], p2[0]]
+        # line = cv2.circle(canny, (round(t2), round(t1)), radius=5, color=(255, 0, 0), thickness=-1)
+        # test1 = [p1[1], p1[0]]
+        # test2 = [p2[1], p2[0]]
         # line = cv2.line(canny, test1, [m2, m1], (255, 255, 255))
         # line = cv2.line(canny, [m2, m1], test2, (255, 255, 255))
-
+        #
         # line = cv2.line(canny, start, [m2, m1], (255, 255, 255))
         #
         # plt.subplot(1, 1, 1), plt.imshow(line)
@@ -118,7 +122,7 @@ if __name__ == '__main__':
         for i in range(cols):
             x = 10*i
             y = x * (t2 - m2) / (t1 - m1) + (m2 * t1 - m1 * t2) / (t1 - m1)
-            y = int(y/10)
+            y = round(y/10)
             lines.append([(t2 - m2) / (t1 - m1), (m2 * t1 - m1 * t2) / (t1 - m1)])
             if 0 <= y < rows:
                 accumulatorArray[i][y] += 1
@@ -163,11 +167,21 @@ if __name__ == '__main__':
     print(canny.shape[0])
     print('shape[1] = ')
     print(canny.shape[1])
-    x = np.linspace(0, 40, canny.shape[1])
-    for i in range(2):
-        plt.imshow(newimage, 'gray')
-        y = lines[i][0]*x + lines[i][1]
-        plt.plot(x, y, '-r', label='y=2x+1')
-    plt.show()
+
+    voted = []
+    accumulator3D = []
+    for center in centers:
+        for i in range(len(indices)):
+            if math.sqrt((indices[i][0][0] - center[0])**2 + (indices[i][0][1] - center[1])**2) <= 200:
+                voted.append(indices[i][0])
+            if math.sqrt((indices[i][1][0] - center[0])**2 + (indices[i][1][1] - center[1])**2) <= 200:
+                voted.append(indices[i][1])
+    print("len of voted = ")
+    print(len(voted))
+    for coordinates in voted:
+        for b in range(3):
+            for d in range(3):
+                c = -coordinates[0]**2 - b*(coordinates[1]**2) - 2*d*coordinates[0]*coordinates[1]
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
